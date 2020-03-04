b0a24b39a9d76f5116131600c10f23fa4395a341
github username <github email address>
2020-03-04 10:27:25 -0700

    Table updates

--- db/migrate/20200303231414_create_klasses_table.rb
+++ db/migrate/20200303231414_create_klasses_table.rb
@@ -2,6 +2,7 @@ class CreateKlassesTable < ActiveRecord::Migration[6.0]
   def change
     create_table klasses do |t|
       t.string :klass_name
- 
+      t.string :weapon
+      t.string :item 
   end
 end
