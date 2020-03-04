ea90357dad1661c49aa5bfd2f3e6e3091a58f65e
github username <github email address>
2020-03-04 10:25:55 -0700

    More updates to tables

--- db/migrate/20200303231348_create_races_table.rb
+++ db/migrate/20200303231348_create_races_table.rb
@@ -2,11 +2,5 @@ class CreateRacesTable < ActiveRecord::Migration[6.0]
   def change
     creat_table races do |t|
       t.string :race_name
-      t.integer :strength
-      t.integer :dexterity
-      t.integer :constitution
-      t.integer :intelligence
-      t.integer :wisdom
-      t.integer :charisma
   end
 end
--- db/migrate/20200303231414_create_klasses_table.rb
+++ db/migrate/20200303231414_create_klasses_table.rb
@@ -2,7 +2,6 @@ class CreateKlassesTable < ActiveRecord::Migration[6.0]
   def change
     create_table klasses do |t|
       t.string :klass_name
-      t.string :weapon
-      t.string :item 
+ 
   end
 end
