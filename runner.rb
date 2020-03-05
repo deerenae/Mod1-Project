b650ccdc862b17cb5607847de01ed4558ecea986
Jeremy <jerome215@comcast.net>
2020-03-05 12:24:28 -0700

    The character can now be created.

--- runner.rb
+++ runner.rb
@@ -1,6 +1,13 @@
 require_relative 'config/environment'
+require_relative 'lib/models/cli'
 require 'pry'
 
 cli = Cli.new
 
 cli.welcome
+cli.name_choice
+cli.race_choice
+cli.klass_choice
+cli.attribute_roller
+cli.attribute_adder
+cli.character_review
